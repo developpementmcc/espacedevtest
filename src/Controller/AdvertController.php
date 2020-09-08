d3e1d03b0f03b5c7b28774492f202d590bcee122
siham diraa <gitignore>
2020-09-08 17:07:39 +0000

    CH. le routeur symfony

--- src/Controller/AdvertController.php
+++ src/Controller/AdvertController.php
@@ -10,10 +10,19 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Twig\Environment;
+use Symfony\Component\Routing\Annotation\Route;
 
+
+/**
+ * @Route("/advert")
+ */
 class  AdvertController
 {
-    
+    /**
+     * @Route("/",name="oc_advert_index")
+     * @param Environment $twig
+     * @return Response
+     */
     public function index(Environment $twig)
     {
         //$content = "<html><body>Notre propre Hello World !<body></html>";
@@ -34,4 +43,32 @@ class  AdvertController
         
     }
     
+    /**
+     * @Route("/view/{id}", name="oc_advert_view")
+     */
+    public function view($id)
+    {
+      // $id vaut 5 si l'URL appelée est /advert/view/5
+
+      return new Response("Affichage de l'annonce d'id : ".$id);
+      
+    }
+    
+    
+
+
+    /**
+    * @Route("/view/{year}/{slug}.{_format}", name="oc_advert_view_slug", requirements={
+    *   "year"   = "\d{4}",
+    *   "_format" = "html|xml"
+    * }, defaults={"_format" = "html"})
+    */
+    public function viewSlug($slug, $year, $_format)
+    {
+        return new Response(
+          "On pourrait afficher l'annonce correspondant au
+          slug '".$slug."', créée en ".$year." et au format ".$_format."."
+        );
+    }
+
 }
